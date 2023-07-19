9d7f17699015c3c3d79226690e5c747a35d50544
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-19 21:22:57 +0300

    Added route guards with a check if current user is logged in.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import type {RouteLocationNormalized} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
+import axios from "axios";
+
+interface ToInterface extends RouteLocationNormalized {
+    meta: { requiresAuth: boolean }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +14,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: false }
     },
     {
       path: '/about',
@@ -16,14 +23,32 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AboutView.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/login',
       name: 'Login',
-      component: LoginView
+      component: LoginView,
+      meta: { requiresAuth: false }
     },
   ]
 })
+
+router.beforeEach(async (to: ToInterface) => {
+  let isLoggedIn;
+  await axios.head('http://localhost/api/me').then(() => {
+    isLoggedIn = true;
+  }).catch(() => {
+    isLoggedIn = false;
+  });
+
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    router.push('/login');
+  }
+  else if (!to.meta.requiresAuth && isLoggedIn) {
+    router.push('/');
+  }
+});
 
 export default router
