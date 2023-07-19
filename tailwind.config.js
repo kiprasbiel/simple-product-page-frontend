b71373cd6b219a8ffa220608935bbc9349306b57
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-19 19:39:30 +0300

    Installed and configured Tailwind css framework.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/components/**/*.vue",
+    "./src/*.vue",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
