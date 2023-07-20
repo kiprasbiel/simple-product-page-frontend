9463ec170f07a59848923f0debb817ea9844452c
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-20 20:33:44 +0300

    Created a new component for displaying error messages. Added it to Login page.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import {Ref, ref} from "vue";
   import axios from "axios";
+import ErrorMessage from "@/components/errorMessage.vue";
 
   const email: Ref<string> = ref('');
   const password: Ref<string> = ref('');
+  const message: Ref<string> = ref('');
 
   function login() {
     axios.post('http://localhost/api/login', {
@@ -12,6 +14,9 @@ import {Ref, ref} from "vue";
     }).then((response) => {
       if(response.data.token){
         localStorage.setItem("kinfirm-token", response.data.token);
+      }
+      else {
+        message.value = response.data.message;
       }
     })
   }
@@ -24,12 +29,14 @@ import {Ref, ref} from "vue";
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
     </div>
 
+
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <ErrorMessage v-if="message" :message="message"/>
       <form class="space-y-6">
         <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
           <div class="mt-2">
-            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
@@ -38,7 +45,7 @@ import {Ref, ref} from "vue";
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
