fa339545bb73fb919b686febb482d27f0ed124ca
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-20 21:37:50 +0300

    Refactored Login page input fields to separate component for better reusability

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,6 +2,7 @@
 import {Ref, ref} from "vue";
   import axios from "axios";
 import ErrorMessage from "@/components/errorMessage.vue";
+import Input from "@/components/Input.vue";
 
   const email: Ref<string> = ref('');
   const password: Ref<string> = ref('');
@@ -33,21 +34,14 @@ import ErrorMessage from "@/components/errorMessage.vue";
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <ErrorMessage v-if="message" :message="message"/>
       <form class="space-y-6">
-        <div>
-          <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-          <div class="mt-2">
-            <input v-model="email" id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
 
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-          </div>
-          <div class="mt-2">
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-          </div>
-        </div>
+        <Input v-model="email" id="email" name="email" type="email" required="true">
+          <template v-slot:title>Email</template>
+        </Input>
+
+        <Input v-model="password" id="password" name="password" type="password" required="true">
+          <template v-slot:title>Password</template>
+        </Input>
 
         <div>
           <button @click="login()" type="button" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
