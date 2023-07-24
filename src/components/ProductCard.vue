556ccdaac418fad59de5ea484c0ab1f1b751a1cf
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-07-24 20:36:53 +0300

    Created new Product View with Product Card component.

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+interface productResponse {
+  id: Number,
+  SKU: String,
+  size: String,
+  photo_url: String,
+  created_at: String
+}
+
+defineProps<productResponse>();
+
+</script>
+
+<template>
+  <div class="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+    <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+      <img class="object-cover" :src="photo_url" alt="product image" />
+    </a>
+    <div class="mt-4 px-5 pb-5">
+      <a href="#">
+        <h5 class="text-xl tracking-tight text-slate-900">{{ SKU }}</h5>
+      </a>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
