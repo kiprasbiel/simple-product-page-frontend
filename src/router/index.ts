b0f3fb5ec3213d8164e232c932718974c7982133
Kipras Bielinskas <kipras.bielinskas@kantar.com>
2023-08-02 09:12:18 +0300

    Removed redundant home and about views and routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import type {RouteLocationNormalized} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView.vue";
 import axios from "axios";
 import RegisterView from "@/views/RegisterView.vue";
@@ -14,21 +13,6 @@ interface ToInterface extends RouteLocationNormalized {
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: { requiresAuth: false }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      meta: { requiresAuth: true }
-    },
     {
       path: '/login',
       name: 'Login',
